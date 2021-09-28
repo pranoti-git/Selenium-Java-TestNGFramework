d638ba9d2d2065dfd60c1b9f9ad00963894fc686
prashantnimje <prashantnimje1121@gmail.com>
2021-09-28 21:18:51 +0530

    adding options in excel

--- src/test/java/utils/listeners/ListenerRetry.java
+++ src/test/java/utils/listeners/ListenerRetry.java
@@ -4,7 +4,7 @@ import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 
 public class ListenerRetry implements IRetryAnalyzer {
-    private int count = 0;                                   //Initialising count to 0
+    private int count = 1;                                   //Initialising count to 1
     private static int maxTry = 3;                           //Setting maxTry i.e number of times failed test should retry
 
     @Override
